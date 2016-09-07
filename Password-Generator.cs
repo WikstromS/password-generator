926f6fb5bd376c89aafcee96e61edd0e256c7b64
Santeri Wikström <wikke93@gmail.com>
2016-09-07 22:23:39 +0300

    lisätty ominaisuuksia ja laitettu main loop

--- Password-Generator.cs
+++ Password-Generator.cs
@@ -4,7 +4,7 @@ using System.IO;
 
 public class laskin
 {
-
+     // moduuli luo halutun pituisen salasanan sallituista kirjaimista.
     public static string generoi(int pituus)
     {
         string sallitut = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@$?_-";
@@ -21,15 +21,66 @@ public class laskin
 
     public static void Main(string[] args)
     {
-       Console.WriteLine("Hei! Tervetuloa käyttämään ohjelmaa.\nKuinka pitkän salasanan haluat? ");
-       // Otetaan käyttäjältä syöte, kuinka pitkän salasanan hän haluaa.
-       string valinta = Console.ReadLine(); 
-       int pituus =  int.Parse(valinta);
-  
+        
 
-        generoi(pituus);
+        Console.WriteLine("Hei! Tervetuloa käyttämään ohjelmaa!\n");
 
-        Console.WriteLine(generoi(pituus));
+        int valinta = 0;
+        while( valinta != 6){               // While looppi pyörittää ohjelman ydintä
+
+            Console.WriteLine("Mitä haluat tehdä?");
+            Console.WriteLine("(1)Generoi uusi salasana");
+            Console.WriteLine("(2)Tallenna salasana tiedostoon");
+            Console.WriteLine("(3)Poista tiedosto");
+            Console.WriteLine("(4)");
+            Console.WriteLine("(5)");
+            Console.WriteLine("(6)Lopettaa ohjelman");
+
+           string syote = Console.ReadLine();
+           valinta = int.Parse(syote);
+
+
+            if (valinta == 1)
+            {
+                Console.WriteLine("Kuinka pitkän salasanan haluat?");
+                string syote2 = Console.ReadLine();
+                int pituus = int.Parse(syote2);
+                string uusiSalasana = (generoi(pituus));
+                Console.WriteLine(generoi(pituus));
+                
+            }
+
+            if (valinta == 2)
+            {
+                Console.WriteLine("Syötä tiedosto polku mihin haluat tallentaa salasanan");
+                string tiedosto = Console.ReadLine();
+
+            }
+
+            if (valinta == 3)
+            {
+
+            }
+
+            if (valinta == 4)
+            {
+
+            }
+
+            if (valinta == 5)
+            {
+
+            }
+
+            if (valinta == 6)
+            {
+                Console.WriteLine("Kiitos kun käytit ohjelmaa!");
+            }
+                
+            
+         
+        }
+    
 
 
     }
