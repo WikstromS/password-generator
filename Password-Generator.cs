df79d6221e5a8c4e86b5428c296ab1ea76ccfefb
Santeri Wikström <wikke93@gmail.com>
2016-09-07 21:19:09 +0300

    Raaka pohja ohjelmalle

---
+++ Password-Generator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+
+public class laskin
+{
+
+    public static string generoi(int pituus)
+    {
+        string sallitut = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@$?_-";
+        char[] kirjaimet = new char[pituus];
+        Random rand = new Random();
+
+        for(int i = 0; i < pituus; i++)
+        {
+            kirjaimet[i] = sallitut[rand.Next(0, sallitut.Length)];
+        }
+        return new string(kirjaimet);
+    }
+
+
+    public static void Main(string[] args)
+    {
+       Console.WriteLine("Hei! Tervetuloa käyttämään ohjelmaa.\nKuinka pitkän salasanan haluat? ");
+       string valinta = Console.ReadLine();
+       int pituus =  int.Parse(valinta);
+        
+
+        generoi(pituus);
+
+        Console.WriteLine(generoi(pituus));
+
+
+    }
+}
