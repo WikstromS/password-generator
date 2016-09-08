2a3b8305adbcd44a2d172f40f5aef785facbbfcd
WikstromS <wikke93@gmail.com>
2016-09-08 08:46:00 +0300

    Lisäyksiä koodiin

--- Password-Generator.cs
+++ Password-Generator.cs
@@ -4,14 +4,16 @@ using System.IO;
 
 public class laskin
 {
-     // moduuli luo halutun pituisen salasanan sallituista kirjaimista.
+    public static string uusiSalasana { get; private set; }
+
+    // moduuli luo halutun pituisen salasanan sallituista kirjaimista.
     public static string generoi(int pituus)
     {
         string sallitut = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@$?_-";
         char[] kirjaimet = new char[pituus];
         Random rand = new Random();
 
-        for(int i = 0; i < pituus; i++)
+        for (int i = 0; i < pituus; i++)
         {
             kirjaimet[i] = sallitut[rand.Next(0, sallitut.Length)];
         }
@@ -21,39 +23,45 @@ public class laskin
 
     public static void Main(string[] args)
     {
-        
+
 
         Console.WriteLine("Hei! Tervetuloa käyttämään ohjelmaa!\n");
 
         int valinta = 0;
-        while( valinta != 6){               // While looppi pyörittää ohjelman ydintä
+        while (valinta != 6)
+        {               // While looppi pyörittää ohjelman ydintä
 
             Console.WriteLine("Mitä haluat tehdä?");
             Console.WriteLine("(1)Generoi uusi salasana");
-            Console.WriteLine("(2)Tallenna salasana tiedostoon");
+            Console.WriteLine("(2)Tallenna salasana tiedostoon");           //salasana tallennetaan oletustiedostoon. Oletustiedostoa voisi muuttaa.
             Console.WriteLine("(3)Poista tiedosto");
-            Console.WriteLine("(4)");
+            Console.WriteLine("(4)Muuta tiedoston sijaintia");
             Console.WriteLine("(5)");
             Console.WriteLine("(6)Lopettaa ohjelman");
+            if (uusiSalasana == null)
+            {
+                Console.WriteLine("Salasanaa ei ole tällä hetkellä generoitu");
+            }
+            else
+                Console.WriteLine("Generoitu salasana on: " + uusiSalasana +"\n");
 
-           string syote = Console.ReadLine();
-           valinta = int.Parse(syote);
+            string syote = Console.ReadLine();
+            valinta = int.Parse(syote);
 
 
             if (valinta == 1)
             {
-                Console.WriteLine("Kuinka pitkän salasanan haluat?");
+                Console.WriteLine("Kuinka pitkän salasanan haluat?\n");
                 string syote2 = Console.ReadLine();
                 int pituus = int.Parse(syote2);
-                string uusiSalasana = (generoi(pituus));
-                Console.WriteLine(generoi(pituus));
-                
+                uusiSalasana = (generoi(pituus));
+                Console.WriteLine("Generoitu salasana on: " + generoi(pituus));
+
             }
 
             if (valinta == 2)
             {
-                Console.WriteLine("Syötä tiedosto polku mihin haluat tallentaa salasanan");
-                string tiedosto = Console.ReadLine();
+
 
             }
 
@@ -64,7 +72,8 @@ public class laskin
 
             if (valinta == 4)
             {
-
+                Console.WriteLine("Syötä tiedosto polku mihin haluat tallentaa salasanan");
+                string tiedosto = Console.ReadLine();
             }
 
             if (valinta == 5)
@@ -76,11 +85,11 @@ public class laskin
             {
                 Console.WriteLine("Kiitos kun käytit ohjelmaa!");
             }
-                
-            
-         
+
+
+
         }
-    
+
 
 
     }
