82a49887e0d8575dd4eea2f3bf23702a6ff47372
Santeri Wikström <wikke93@gmail.com>
2016-10-10 15:42:55 +0300

    fixed encryption

--- WpfApplication2/WpfApplication2/MainWindow.xaml.cs
+++ WpfApplication2/WpfApplication2/MainWindow.xaml.cs
@@ -32,6 +32,7 @@ namespace WpfApplication2
         public string nums = "0123456789";
         public static int N;
         public static string passw;
+        
 
         static readonly string PasswordHash = "P@@Sw0rd";
         static readonly string SaltKey = "S@LT&KEY";
@@ -41,7 +42,7 @@ namespace WpfApplication2
         {
             
             InitializeComponent();                                      // TÄYTYY KEKSIÄ PAREMPI VAIHTOEHTO
-            new ImageBrush(new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), "C://Users//wiku//password-generator//WpfApplication2//lataus.jpg")));
+            new ImageBrush(new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), "C://Users//Santun//Password-Generator//WpfApplication2//lataus.jpg")));
 
         }
         
@@ -184,44 +185,12 @@ namespace WpfApplication2
                 // Lähettää sähköpostin kun nappia painaa! :))
         private void BtnSend_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                MailMessage mail = new MailMessage();
-                //SMTP address and port here
-                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com", 587 );
-                //Put the email address
-                mail.From = new MailAddress("wikke93@gmail.com");
-                //Put the email where you want to send.
-                mail.To.Add("kekkosenseppo@gmail.com");
 
-                mail.Subject = "Generoitu Salasana";
+            Window1 win2 = new Window1();
+            win2.Show();
 
-                StringBuilder sbBody = new StringBuilder();
 
-                sbBody.AppendLine("Hei käyttäjä!");
-                sbBody.AppendLine("Tässä salasanasi!");
-                sbBody.AppendLine(passw);
-                
-
-                mail.Body = sbBody.ToString();
-
-                //mail.Attachments.Add(liite) tällä tavalla salasanan lähetys?
-
-                //username and password!
-
-                SmtpServer.Credentials = new System.Net.NetworkCredential("pwgenerator93@gmail.com", "aspretto");
-                //Set Smtp server port
-                SmtpServer.Port = 587;
-                SmtpServer.EnableSsl = true;
-
-                SmtpServer.Send(mail);
-                MessageBox.Show("Salasana lähetettiin :) ");
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
+      
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
@@ -232,6 +201,12 @@ namespace WpfApplication2
         private void textBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 win3 = new Window2();
+            win3.Show();
         }
     }
 }
---
+++ WpfApplication2/WpfApplication2/Window1.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+
+        public string userMail;
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(WpfApplication2.MainWindow.passw);
+            
+                 try
+            {
+                MailMessage mail = new MailMessage();
+                //SMTP address and port here
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com", 587 );
+                //Put the email address
+                mail.From = new MailAddress("wikke93@gmail.com");
+                //Put the email where you want to send.
+                mail.To.Add(userMail);                 //pwgenerator93@gmail.com on testi boxi
+
+                mail.Subject = "Generoitu Salasana";
+
+                StringBuilder sbBody = new StringBuilder();
+
+                sbBody.AppendLine("Hei käyttäjä!");
+                sbBody.AppendLine("Tässä salasanasi!");
+                sbBody.AppendLine(WpfApplication2.MainWindow.passw);
+                sbBody.AppendLine(WpfApplication2.MainWindow.Encrypt(WpfApplication2.MainWindow.passw));
+                
+                
+
+                mail.Body = sbBody.ToString();
+
+                //mail.Attachments.Add(liite) tällä tavalla salasanan lähetys?
+
+                //username and password!
+
+                SmtpServer.Credentials = new System.Net.NetworkCredential("pwgenerator93@gmail.com", "aspretto");
+                //Set Smtp server port
+                SmtpServer.Port = 587;
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                MessageBox.Show("Salasana lähetettiin :) ");
+
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            } 
+            this.Close(); 
+            
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            userMail = textBox.Text;
+        }
+    }
+}
